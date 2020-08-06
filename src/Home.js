66b49ca0881ed2a6547484da3db0ccf1716a3006
David Dios <david.dios@loveholidays.com>
2020-08-06 16:57:24 +0200

    cleanup

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { FetchingComponent } from './FetchingComponent';
 
-export const Home = () => {
-  console.log('a');
+export const Home = () => (
+  <React.Fragment>
+    <h1
+      sx={{
+        color: 'red'
+      }}
+    >
+      Hello world
+    </h1>
 
-  return (
-    <React.Fragment>
-      <h1
-        sx={{
-          color: 'red'
-        }}
-      >
-        Hello world
-      </h1>
-
-      <FetchingComponent />
-    </React.Fragment>
-  )
-}
-
+    <FetchingComponent />
+  </React.Fragment>
+);
